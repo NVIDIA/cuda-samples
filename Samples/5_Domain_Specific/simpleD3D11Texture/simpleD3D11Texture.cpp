718fe6486d68bbf7d5b56c4eaa8d4a8cfff01032
shawnz <shawnz@nvidia.com>
2025-04-02 15:10:29 +0800

    Bug 5199167: Adjust the include header files sequence for simpleD3D11/simpleD3D11Texture

--- Samples/5_Domain_Specific/simpleD3D11Texture/simpleD3D11Texture.cpp
+++ Samples/5_Domain_Specific/simpleD3D11Texture/simpleD3D11Texture.cpp
@@ -31,8 +31,8 @@
 
 #pragma warning(disable : 4312)
 
-#include <mmsystem.h>
 #include <windows.h>
+#include <mmsystem.h>
 
 // This header inclues all the necessary D3D11 and CUDA includes
 #include <cuda_d3d11_interop.h>
