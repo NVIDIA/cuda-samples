235369d4df58b7bcd5da079388fd15162c238bee
Shih-Yuan Lee (FourDollars) <fourdollars@gmail.com>
2025-06-26 20:36:09 +0800

    Fix the redeclaration against GLES2 and GLES3.

--- Common/GL/glext.h
+++ Common/GL/glext.h
@@ -3818,7 +3818,9 @@ typedef void (APIENTRYP PFNGLGETVERTEXATTRIBPOINTERVPROC)(GLuint index, GLenum p
 typedef GLboolean(APIENTRYP PFNGLISPROGRAMPROC)(GLuint program);
 typedef GLboolean(APIENTRYP PFNGLISSHADERPROC)(GLuint shader);
 typedef void (APIENTRYP PFNGLLINKPROGRAMPROC)(GLuint program);
+#if !defined(GL_ES_VERSION_2_0) && !defined(GL_ES_VERSION_3_0)
 typedef void (APIENTRYP PFNGLSHADERSOURCEPROC)(GLuint shader, GLsizei count, const GLchar **string, const GLint *length);
+#endif
 typedef void (APIENTRYP PFNGLUSEPROGRAMPROC)(GLuint program);
 typedef void (APIENTRYP PFNGLUNIFORM1FPROC)(GLint location, GLfloat v0);
 typedef void (APIENTRYP PFNGLUNIFORM2FPROC)(GLint location, GLfloat v0, GLfloat v1);
