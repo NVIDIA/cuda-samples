6c97408018273cda5ea1bef663502b3f9334f47a
Nicholas Angle <nangle@nvidia.com>
2025-12-11 16:21:08 -0800

    Fix formatting with pre-commit

--- Samples/3_CUDA_Features/trimNotification/trimNotification.cpp
+++ Samples/3_CUDA_Features/trimNotification/trimNotification.cpp
@@ -25,9 +25,8 @@
  * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
-#include <cuda_runtime_api.h>
-
 #include <chrono>
+#include <cuda_runtime_api.h>
 #include <mutex>
 #include <span>
 #include <stdio.h>
@@ -39,49 +38,50 @@ constexpr uint64_t maxSteps     = 500;
 
 // This sample uses the cudaDeviceRegisterAsyncNotification API to allocate as much video memory as it can.
 // A notification to free some video memory is sent whenever the application goes over its budget. If multiple
-// instances of this program are launched, all instances will likely end up with similar budgets and in 
+// instances of this program are launched, all instances will likely end up with similar budgets and in
 // steady-state will have similar amounts of video memory allocated.
 
 int main()
 {
     // This structure will be our 'userData' to pass into the callback. This will give the
     // callback state that it can affect without needing to use globals.
-    struct MemoryInfo {
-        std::vector<int*> memblocks;
-        std::mutex mutex;
-        bool allowedToGrow = true;
+    struct MemoryInfo
+    {
+        std::vector<int *> memblocks;
+        std::mutex         mutex;
+        bool               allowedToGrow = true;
     } memoryInfo;
 
     // The function that we will register as a callback. This could also be a standalone function
     // instead of a lambda, but, since it is only referenced here, we can use a lambda to avoid
     // using the global namespace.
-    cudaAsyncCallback basicUserCallback = [](cudaAsyncNotificationInfo_t* notificationInfo, void* userData, cudaAsyncCallbackHandle_t callback)
-    {
-        MemoryInfo* memoryInfo = (MemoryInfo*)userData;
+    cudaAsyncCallback basicUserCallback =
+        [](cudaAsyncNotificationInfo_t *notificationInfo, void *userData, cudaAsyncCallbackHandle_t callback) {
+            MemoryInfo *memoryInfo = (MemoryInfo *)userData;
 
-        // Must check the type before accessing the info member of cudaAsyncNotificationInfo_t.
-        // Otherwise, we could misinterpret notificationInfo->info if a different type of
-        // notification is sent.
-        if (notificationInfo->type == cudaAsyncNotificationTypeOverBudget) {
-            printf("Asked to free %lld bytes of video memory\n", notificationInfo->info.overBudget.bytesOverBudget);
-            uint64_t numBlocksToFree = (notificationInfo->info.overBudget.bytesOverBudget / memblockSize) + 1;
+            // Must check the type before accessing the info member of cudaAsyncNotificationInfo_t.
+            // Otherwise, we could misinterpret notificationInfo->info if a different type of
+            // notification is sent.
+            if (notificationInfo->type == cudaAsyncNotificationTypeOverBudget) {
+                printf("Asked to free %lld bytes of video memory\n", notificationInfo->info.overBudget.bytesOverBudget);
+                uint64_t numBlocksToFree = (notificationInfo->info.overBudget.bytesOverBudget / memblockSize) + 1;
 
-            // The async notification will be on a separate thread, so shared data should be synchronized.
-            std::scoped_lock lock(memoryInfo->mutex);
-            memoryInfo->allowedToGrow = false;
-            
-            // Free the required number of memblocks
-            std::span<int*> blocksToFree(memoryInfo->memblocks.end() - numBlocksToFree, numBlocksToFree);
+                // The async notification will be on a separate thread, so shared data should be synchronized.
+                std::scoped_lock lock(memoryInfo->mutex);
+                memoryInfo->allowedToGrow = false;
 
-            for (auto& block : blocksToFree) {
-                cudaFree(block);
-                block = nullptr;
+                // Free the required number of memblocks
+                std::span<int *> blocksToFree(memoryInfo->memblocks.end() - numBlocksToFree, numBlocksToFree);
+
+                for (auto &block : blocksToFree) {
+                    cudaFree(block);
+                    block = nullptr;
+                }
+
+                // Shrink the vector to remove the freed blocks
+                std::erase(memoryInfo->memblocks, nullptr);
             }
-
-            // Shrink the vector to remove the freed blocks
-            std::erase(memoryInfo->memblocks, nullptr);
-        }
-    };
+        };
 
     // Initialize CUDA
     const int cudaDevice = 0;
@@ -91,27 +91,25 @@ int main()
     // cudaDeviceUnregisterAsyncNotification and to identify which callback registration a given
     // notification corresponds to.
     cudaAsyncCallbackHandle_t callback;
-    cudaDeviceRegisterAsyncNotification(cudaDevice, basicUserCallback, (void*)&memoryInfo, &callback);
+    cudaDeviceRegisterAsyncNotification(cudaDevice, basicUserCallback, (void *)&memoryInfo, &callback);
 
     // Attempt to allocate a block of memory, then sleep before repeating.
     uint64_t stepCounter = 0;
-    while (stepCounter < maxSteps)
-    {
+    while (stepCounter < maxSteps) {
         {
             // The async notification will be on a separate thread, so shared data should be synchronized.
             std::scoped_lock lock(memoryInfo.mutex);
 
-            if (memoryInfo.allowedToGrow)
-            {
-                int* newMemblock;
-                cudaError_t cudaStatus = cudaMalloc((void**)&newMemblock, memblockSize);
+            if (memoryInfo.allowedToGrow) {
+                int        *newMemblock;
+                cudaError_t cudaStatus = cudaMalloc((void **)&newMemblock, memblockSize);
                 if (cudaStatus != cudaSuccess) {
                     fprintf(stderr, "cudaMalloc failed!");
                 }
                 memoryInfo.memblocks.push_back(newMemblock);
             }
         }
-        
+
         std::this_thread::sleep_for(std::chrono::milliseconds(stepTime));
         stepCounter++;
     }
